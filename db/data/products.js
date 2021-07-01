d9342645eade80a8639130fef6617d53e5bd838b
Aya Mallah <ayamallah@hotmail.co.uk>
2021-07-01 12:23:08 +0100

    product-seed-complete

--- db/data/products.js
+++ db/data/products.js
@@ -6,7 +6,7 @@ export default [
     ingredient: ['100% Italian Beef', 'Finest San Marzano Tomato Sauce'],
     image: 'https://www.seriouseats.com/thmb/rXCkXUKlw_fUEvYVTltf8aHOMbc=/1500x1125/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__20150113-lasagna-napoletana-meatball-ragu-italian-food-lab-02-6d11889aad024581b357abf8e407304f.jpg',
     typeAnimal: 'Dog',
-    allergens: ['poo', 'wee'],
+    allergens: ['none'],
     storage: 'Delivered freshly frozen. Store in the fridge or freezer. Once defrosted use within 7 days. Food can be gently heated.',
     typeProduct: 'Main Meal',
     age: 'Adult',
