86d68e50cf3f946dd5e20f6762a4374151c56429
Arjun Doel <arjundoel@gmail.com>
2021-07-05 16:11:20 +0100

    styling layout

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
 
 const Register = () => {
   const history = useHistory()
@@ -18,23 +18,22 @@ const Register = () => {
   })
 
   const [errors, setErrors] = useState({
-    username: {  },
-    email: {  },
-    password: {  },
-    passwordConfirmation: {  },
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
   })
 
   //*Get userInput
   const handleUserData = e => {
     const getUserData = { ...formData, [e.target.name]: e.target.value }
-    console.log('handleUsers->', getUserData)
-    // const newErrors = { ...errors, [e.target.name]: '' }
+    const newErrors = { ...errors, [e.target.name]: '' }
     setFormData(getUserData)
-    // setErrors(newErrors)
+    setErrors(newErrors)
   }
 
-  
 
+  //* Submit form as post request to backend
   const submitForm = async e => {
     e.preventDefault()
     try {
@@ -42,53 +41,49 @@ const Register = () => {
       history.push('/login')
     } catch (err) {
       console.log(err)
-      const newErrors = err.response.data.errors
-      // const errorsTwo = { ...errors, [newErrors.username]: newErrors.username.message  }
-      console.log(newErrors.email)
-      setErrors(newErrors)
-      // console.log(err.response.data.errors.passwordConfirmation.message)
+      setErrors(err.response.data.errors)
     }
   }
 
-  console.log(errors)
 
-  // console.log(formData)
-  // console.log(errors.passwordConfirmation.messaage)
 
 
   return (
     <>
-      <h1 className="text-center">Register With Us!</h1>
+
+      <h1 className="register">Register With Us!</h1>
       <Container fluid="md" className="center-height">
         <Row className="justify-content-md-center">
-          <Form onSubmit={submitForm}>
+          <Col >
+            <Form onSubmit={submitForm} className='register-form'>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Username</Form.Label>
-              <Form.Control name="username" type="text" placeholder="Enter username" value={formData.username} onChange={handleUserData} />
-              {/* {errors.username.message && <Form.Text className="text-muted">{errors.username.message}</Form.Text>} */}
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Username</Form.Label>
+                <Form.Control name="username" type="text" placeholder="Enter username" value={formData.username} onChange={handleUserData} />
+                {errors.username && <Form.Text className="text-muted">{errors.username.message}</Form.Text>}
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control name="email" type="email" placeholder="Enter email" value={formData.email} onChange={handleUserData} />
-              {/* {errors.email.message && <Form.Text className="text-muted">{errors.username.message}</Form.Text>} */}
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control name="email" type="email" placeholder="Enter email" value={formData.email} onChange={handleUserData} />
+                {errors.email && <Form.Text className="text-muted">{errors.email.message}</Form.Text>}
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control name="password" type="password" placeholder="Password" value={formData.password} onChange={handleUserData} />
-              {/* {errors.password.message && <Form.Text className="text-muted">{errors.username.message}</Form.Text>} */}
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control name="password" type="password" placeholder="Password" value={formData.password} onChange={handleUserData} />
+                {errors.password && <Form.Text className="text-muted">{errors.password.message}</Form.Text>}
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Please confirm your password</Form.Label>
-              <Form.Control name="passwordConfirmation" type="password" placeholder="Password Confirmation" value={formData.passwordConfirmation} onChange={handleUserData} />
-              {/* { errors.passwordConfirmation.message ? <Form.Text className="text-muted">{errors.username.message}</Form.Text> : false } */}
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Please confirm your password</Form.Label>
+                <Form.Control name="passwordConfirmation" type="password" placeholder="Password Confirmation" value={formData.passwordConfirmation} onChange={handleUserData} />
+                {errors.passwordConfirmation && <Form.Text className="text-muted">{errors.passwordConfirmation.message}</Form.Text>}
+              </Form.Group>
 
-            <Button variant="primary" type="submit">Submit</Button>
-          </Form>
+              <button type="submit">Submit</button>
+            </Form>
+          </Col>
         </Row>
       </Container>
     </>
