65316a7787aef58c62f07fdd84c7ec6fd955adf2
Arjun Doel <arjundoel@gmail.com>
2021-07-08 19:08:50 +0100

    transition added

--- client/src/components/Popups/Thanks.js
+++ client/src/components/Popups/Thanks.js
@@ -10,7 +10,7 @@ const Thanks = () => {
       <div className="thanks-page">
         <div className="thanks">
           <h1 id="header-thanks">Thanks For Shopping!</h1>
-          <Link to="/products"><p>Continue Shopping</p></Link>
+          <Link to="/products"><p id="continue">Continue Shopping...</p></Link>
         </div>
       </div>
       {/* <img className="party" src="https://res.cloudinary.com/dlj1sbbtb/image/upload/v1625766285/pets-3715733_f2m663.jpg" alt="dog and pet party" /> */}
